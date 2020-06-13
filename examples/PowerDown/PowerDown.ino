fc671ec2a79128e8421be4292aab08e07eed3eb7
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-06-13 15:15:22 +0200

    Add files via upload

---
+++ examples/PowerDown/PowerDown.ino
@@ -0,0 +1,71 @@
+/***************************************************************************
+* Example sketch for the INA219_WE library
+*
+* This sketch shows how to use the INA219 module in continous mode. 
+*  
+* Further information can be found on:
+* http://wolles-elektronikkiste.de/ina219
+* 
+***************************************************************************/
+#include <Wire.h>
+#include <INA226_WE.h>
+#define I2C_ADDRESS 0x40
+
+INA226_WE ina226(I2C_ADDRESS);
+// INA226_WE ina226 = INA226_WE(); // Alternative: sets default address 0x40
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  // default parameters are set - for change check the other examples
+  ina226.init();
+  Serial.println("INA226 Current Sensor Example Sketch - PowerDown");
+  Serial.println("Continous Sampling starts");
+  Serial.println();
+}
+
+void loop() {
+  for(int i=0; i<5; i++){
+    continousSampling();
+    delay(3000);
+  }
+  
+  Serial.println("Power down for 10s");
+  ina226.powerDown();
+  for(int i=0; i<10; i++){
+    Serial.print(".");
+    delay(1000);
+  }
+  
+  Serial.println("Power up!");
+  Serial.println("");
+  ina226.powerUp();
+}
+
+void continousSampling(){
+  float shuntVoltage_mV = 0.0;
+  float loadVoltage_V = 0.0;
+  float busVoltage_V = 0.0;
+  float current_mA = 0.0;
+  float power_mW = 0.0; 
+
+  ina226.readAndClearFlags();
+  shuntVoltage_mV = ina226.getShuntVoltage_mV();
+  busVoltage_V = ina226.getBusVoltage_V();
+  current_mA = ina226.getCurrent_mA();
+  power_mW = ina226.getBusPower();
+  loadVoltage_V  = busVoltage_V + (shuntVoltage_mV/1000);
+  
+  Serial.print("Shunt Voltage [mV]: "); Serial.println(shuntVoltage_mV);
+  Serial.print("Bus Voltage [V]: "); Serial.println(busVoltage_V);
+  Serial.print("Load Voltage [V]: "); Serial.println(loadVoltage_V);
+  Serial.print("Current[mA]: "); Serial.println(current_mA);
+  Serial.print("Bus Power [mW]: "); Serial.println(power_mW);
+  if(!ina226.overflow){
+    Serial.println("Values OK - no overflow");
+  }
+  else{
+    Serial.println("Overflow! Choose higher current range");
+  }
+  Serial.println();
+}
