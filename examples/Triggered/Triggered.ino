198640f4b0fb5bbb49af0bc262da3dc5f8ba6cea
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-05-15 13:12:36 +0200

    Update Triggered.ino

--- examples/Triggered/Triggered.ino
+++ examples/Triggered/Triggered.ino
@@ -14,9 +14,9 @@
 
 /* There are several ways to create your INA226 object:
  * INA226_WE ina226 = INA226_WE()              -> uses Wire / I2C Address = 0x40
- * INA219_WE ina226 = INA226_WE(ICM20948_ADDR) -> uses Wire / I2C_ADDRESS
- * INA219_WE ina226 = INA226_WE(&wire2)        -> uses the TwoWire object wire2 / I2C_ADDRESS
- * INA219_WE ina226 = INA226_WE(&wire2, I2C_ADDRESS) -> all together
+ * INA226_WE ina226 = INA226_WE(ICM20948_ADDR) -> uses Wire / I2C_ADDRESS
+ * INA226_WE ina226 = INA226_WE(&wire2)        -> uses the TwoWire object wire2 / I2C_ADDRESS
+ * INA226_WE ina226 = INA226_WE(&wire2, I2C_ADDRESS) -> all together
  * Successfully tested with two I2C busses on an ESP32
  */
 INA226_WE ina226 = INA226_WE(I2C_ADDRESS);
