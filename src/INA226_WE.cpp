ac9e49fd4d6e35194b9bad72117d85dc93cba97d
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-05-15 13:02:12 +0200

    Add files via upload

--- src/INA226_WE.cpp
+++ src/INA226_WE.cpp
@@ -17,156 +17,170 @@
 #include "INA226_WE.h"
 
 INA226_WE::INA226_WE(int addr){
-	i2cAddress = addr;
+    _wire = &Wire;
+    i2cAddress = addr;   
 }
 
 INA226_WE::INA226_WE(){
-	i2cAddress = 0x40;
+    _wire = &Wire;
+    i2cAddress = 0x40;   
 }
-	
+
+INA226_WE::INA226_WE(TwoWire *w, int addr){
+    _wire = w;
+    i2cAddress = addr; 
+}
+
+INA226_WE::INA226_WE(TwoWire *w){
+    _wire = w;
+    i2cAddress = 0x40;
+}
+
 bool INA226_WE::init(){
-	Wire.beginTransmission(i2cAddress);
-  if(Wire.endTransmission()){
-    return 0;
-  }
-	reset_INA226();
-	setAverage(AVERAGE_1);
-	setConversionTime(CONV_TIME_1100);
-	setMeasureMode(CONTINUOUS);
-	setCurrentRange(MA_800);
-	convAlert = false;
-	limitAlert = false;
-	return 1;
+    _wire->beginTransmission(i2cAddress);
+    if(_wire->endTransmission()){
+        return 0;
+    }
+    reset_INA226();
+    setAverage(AVERAGE_1);
+    setConversionTime(CONV_TIME_1100);
+    setMeasureMode(CONTINUOUS);
+    setCurrentRange(MA_800);
+    convAlert = false;
+    limitAlert = false;
+    corrFactor = 1.0;
+    return 1;
 }
 
 void INA226_WE::reset_INA226(){
-	writeRegister(INA226_CONF_REG, INA226_RST); 
+    writeRegister(INA226_CONF_REG, INA226_RST); 
 }
 
 void INA226_WE::setCorrectionFactor(float corr){
-	calVal *= corr;
-	writeRegister(INA226_CAL_REG, calVal);
+    corrFactor = corr;
+    calVal *= corrFactor;
+    writeRegister(INA226_CAL_REG, calVal);
 }
 
 void INA226_WE::setAverage(INA226_AVERAGES averages){
-	deviceAverages = averages;
-	uint16_t currentConfReg = readRegister(INA226_CONF_REG);
-	currentConfReg &= ~(0x0E00);  
-	currentConfReg |= deviceAverages;
-	writeRegister(INA226_CONF_REG, currentConfReg);
+    deviceAverages = averages;
+    uint16_t currentConfReg = readRegister(INA226_CONF_REG);
+    currentConfReg &= ~(0x0E00);  
+    currentConfReg |= deviceAverages;
+    writeRegister(INA226_CONF_REG, currentConfReg);
 }
 
 void INA226_WE::setConversionTime(INA226_CONV_TIME shuntConvTime, INA226_CONV_TIME busConvTime){
-	uint16_t currentConfReg = readRegister(INA226_CONF_REG);
-	currentConfReg &= ~(0x01C0);  
-	currentConfReg &= ~(0x0038);
-	uint16_t convMask = ((uint16_t)shuntConvTime)<<3;
-	currentConfReg |= convMask;
-	convMask = busConvTime<<6;
-	currentConfReg |= convMask;
-	writeRegister(INA226_CONF_REG, currentConfReg);
+    uint16_t currentConfReg = readRegister(INA226_CONF_REG);
+    currentConfReg &= ~(0x01C0);  
+    currentConfReg &= ~(0x0038);
+    uint16_t convMask = ((uint16_t)shuntConvTime)<<3;
+    currentConfReg |= convMask;
+    convMask = busConvTime<<6;
+    currentConfReg |= convMask;
+    writeRegister(INA226_CONF_REG, currentConfReg);
 }
 
 void INA226_WE::setConversionTime(INA226_CONV_TIME convTime){
-	setConversionTime(convTime, convTime);
+    setConversionTime(convTime, convTime);
 }
 
 void INA226_WE::setMeasureMode(INA226_MEASURE_MODE mode){
-	deviceMeasureMode = mode;
-	uint16_t currentConfReg = readRegister(INA226_CONF_REG);
-	currentConfReg &= ~(0x0007);
-	currentConfReg |= deviceMeasureMode;
-	writeRegister(INA226_CONF_REG, currentConfReg);
+    deviceMeasureMode = mode;
+    uint16_t currentConfReg = readRegister(INA226_CONF_REG);
+    currentConfReg &= ~(0x0007);
+    currentConfReg |= deviceMeasureMode;
+    writeRegister(INA226_CONF_REG, currentConfReg);
 }
 
 void INA226_WE::setCurrentRange(INA226_CURRENT_RANGE range){
-	deviceCurrentRange = range; 
-	
-	switch(deviceCurrentRange){
-		case MA_400:
-			calVal = 2560;
-			currentDivider_mA = 50.0;
-			pwrMultiplier_mW = 0.5;
-			break;
-		case MA_800:
-			calVal = 1280;
-			currentDivider_mA = 25.0;
-			pwrMultiplier_mW = 1.0;
-			break;
-	}	
-	writeRegister(INA226_CAL_REG, calVal);			
+    deviceCurrentRange = range; 
+    
+    switch(deviceCurrentRange){
+        case MA_400:
+            calVal = 2560;
+            currentDivider_mA = 50.0;
+            pwrMultiplier_mW = 0.5;
+            break;
+        case MA_800:
+            calVal = 1280;
+            currentDivider_mA = 25.0;
+            pwrMultiplier_mW = 1.0;
+            break;
+    }   
+    writeRegister(INA226_CAL_REG, calVal);          
 }
 
 //set resistor and current range independant. resistor value in ohm, current range in A
 void INA226_WE::setResistorRange(float resistor, float current_range){
-	float current_LSB=current_range/32768.0;
+    float current_LSB=current_range/32768.0;
 
-	calVal = 0.00512/(current_LSB*resistor);
-	currentDivider_mA = 0.001/current_LSB;
-	pwrMultiplier_mW = 1000.0*25.0*current_LSB;
+    calVal = 0.00512/(current_LSB*resistor);
+    currentDivider_mA = 0.001/current_LSB;
+    pwrMultiplier_mW = 1000.0*25.0*current_LSB;
 
-	writeRegister(INA226_CAL_REG, calVal);			
+    writeRegister(INA226_CAL_REG, calVal);          
 }
 
 float INA226_WE::getShuntVoltage_V(){
-	int16_t val;
-	val = (int16_t) readRegister(INA226_SHUNT_REG);
-	return (val * 0.0000025);	
+    int16_t val;
+    val = (int16_t) readRegister(INA226_SHUNT_REG);
+    return (val * 0.0000025 * corrFactor);  
 }
 
 float INA226_WE::getShuntVoltage_mV(){
-	int16_t val;
-	val = (int16_t) readRegister(INA226_SHUNT_REG);
-	return (val * 0.0025);	
+    int16_t val;
+    val = (int16_t) readRegister(INA226_SHUNT_REG);
+    return (val * 0.0025 * corrFactor); 
 }
 
 float INA226_WE::getBusVoltage_V(){
-	uint16_t val;
-	val = readRegister(INA226_BUS_REG);
-	return (val * 0.00125);
+    uint16_t val;
+    val = readRegister(INA226_BUS_REG);
+    return (val * 0.00125);
 }
 
 float INA226_WE::getCurrent_mA(){
-	int16_t val;
-	val = (int16_t)readRegister(INA226_CURRENT_REG);
-	return (val / currentDivider_mA);
+    int16_t val;
+    val = (int16_t)readRegister(INA226_CURRENT_REG);
+    return (val / currentDivider_mA);
 }
 
 float INA226_WE::getCurrent_A() {
-	return (getCurrent_mA()/1000);
+    return (getCurrent_mA()/1000);
 }
 
 float INA226_WE::getBusPower(){
-	uint16_t val;
-	val = readRegister(INA226_PWR_REG);
-	return (val * pwrMultiplier_mW);
+    uint16_t val;
+    val = readRegister(INA226_PWR_REG);
+    return (val * pwrMultiplier_mW);
 }
 
 void INA226_WE::startSingleMeasurement(){
-	uint16_t val = readRegister(INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
-	val = readRegister(INA226_CONF_REG);
-	writeRegister(INA226_CONF_REG, val);		// Starts conversion
-	uint16_t convReady = 0x0000;
-	while(!convReady){
-		convReady = ((readRegister(INA226_MASK_EN_REG)) & 0x0008); // checks if sampling is completed
-	}
+    uint16_t val = readRegister(INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
+    val = readRegister(INA226_CONF_REG);
+    writeRegister(INA226_CONF_REG, val);        // Starts conversion
+    uint16_t convReady = 0x0000;
+    while(!convReady){
+        convReady = ((readRegister(INA226_MASK_EN_REG)) & 0x0008); // checks if sampling is completed
+    }
 }
 
 // Don't wait for conversion to complete
 void INA226_WE::startSingleMeasurementNoWait(){
-	uint16_t val = readRegister(INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
-	val = readRegister(INA226_CONF_REG);
-	writeRegister(INA226_CONF_REG, val);		// Starts conversion
+    uint16_t val = readRegister(INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
+    val = readRegister(INA226_CONF_REG);
+    writeRegister(INA226_CONF_REG, val);        // Starts conversion
 }
 
 void INA226_WE::powerDown(){
-	confRegCopy = readRegister(INA226_CONF_REG);
-	setMeasureMode(POWER_DOWN);
+    confRegCopy = readRegister(INA226_CONF_REG);
+    setMeasureMode(POWER_DOWN);
 }
 
 void INA226_WE::powerUp(){
-	writeRegister(INA226_CONF_REG, confRegCopy);
-    delayMicroseconds(40);	
+    writeRegister(INA226_CONF_REG, confRegCopy);
+    delayMicroseconds(40);  
 }
 
 // Returns 1 if conversion is still ongoing
@@ -175,106 +189,106 @@ bool INA226_WE::isBusy(){
 }
     
 void INA226_WE::waitUntilConversionCompleted(){
-	readRegister(INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
-	uint16_t convReady = 0x0000;
-	while(!convReady){
-		convReady = ((readRegister(INA226_MASK_EN_REG)) & 0x0008); // checks if sampling is completed
-	}
+    readRegister(INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
+    uint16_t convReady = 0x0000;
+    while(!convReady){
+        convReady = ((readRegister(INA226_MASK_EN_REG)) & 0x0008); // checks if sampling is completed
+    }
 }
 
 void INA226_WE::setAlertPinActiveHigh(){
-	uint16_t val = readRegister(INA226_MASK_EN_REG);
-	val |= 0x0002;
-	writeRegister(INA226_MASK_EN_REG, val);
+    uint16_t val = readRegister(INA226_MASK_EN_REG);
+    val |= 0x0002;
+    writeRegister(INA226_MASK_EN_REG, val);
 }
 
 void INA226_WE::enableAlertLatch(){
-	uint16_t val = readRegister(INA226_MASK_EN_REG);
-	val |= 0x0001;
-	writeRegister(INA226_MASK_EN_REG, val);
+    uint16_t val = readRegister(INA226_MASK_EN_REG);
+    val |= 0x0001;
+    writeRegister(INA226_MASK_EN_REG, val);
 }
 
 void INA226_WE::enableConvReadyAlert(){
-	uint16_t val = readRegister(INA226_MASK_EN_REG);
-	val |= 0x0400;
-	writeRegister(INA226_MASK_EN_REG, val);
+    uint16_t val = readRegister(INA226_MASK_EN_REG);
+    val |= 0x0400;
+    writeRegister(INA226_MASK_EN_REG, val);
 }
-	
+    
 void INA226_WE::setAlertType(INA226_ALERT_TYPE type, float limit){
-	deviceAlertType = type;
-	uint16_t alertLimit = 0;
-	
-	switch(deviceAlertType){
-		case SHUNT_OVER:
-			alertLimit = limit * 400;			
-			break;
-		case SHUNT_UNDER:
-			alertLimit = limit * 400; 
-			break;
-		case CURRENT_OVER:
-			deviceAlertType = SHUNT_OVER;
-			alertLimit = limit * 2048 * currentDivider_mA / calVal;
-			break;
-		case CURRENT_UNDER:
-			deviceAlertType = SHUNT_UNDER;
-			alertLimit = limit * 2048 * currentDivider_mA / calVal;
-			break;
-		case BUS_OVER:
-			alertLimit = limit * 800;
-			break;
-		case BUS_UNDER:
-			alertLimit = limit * 800;
-			break;
-		case POWER_OVER:
-			alertLimit = limit / pwrMultiplier_mW;
-			break;
-	}
-	
-	writeRegister(INA226_ALERT_LIMIT_REG, alertLimit);
-	
-	uint16_t value = readRegister(INA226_MASK_EN_REG);
-	value &= ~(0xF800);
-	value |= deviceAlertType;
-	writeRegister(INA226_MASK_EN_REG, value);
-	
+    deviceAlertType = type;
+    uint16_t alertLimit = 0;
+    
+    switch(deviceAlertType){
+        case SHUNT_OVER:
+            alertLimit = limit * 400;           
+            break;
+        case SHUNT_UNDER:
+            alertLimit = limit * 400; 
+            break;
+        case CURRENT_OVER:
+            deviceAlertType = SHUNT_OVER;
+            alertLimit = limit * 2048 * currentDivider_mA / calVal;
+            break;
+        case CURRENT_UNDER:
+            deviceAlertType = SHUNT_UNDER;
+            alertLimit = limit * 2048 * currentDivider_mA / calVal;
+            break;
+        case BUS_OVER:
+            alertLimit = limit * 800;
+            break;
+        case BUS_UNDER:
+            alertLimit = limit * 800;
+            break;
+        case POWER_OVER:
+            alertLimit = limit / pwrMultiplier_mW;
+            break;
+    }
+    
+    writeRegister(INA226_ALERT_LIMIT_REG, alertLimit);
+    
+    uint16_t value = readRegister(INA226_MASK_EN_REG);
+    value &= ~(0xF800);
+    value |= deviceAlertType;
+    writeRegister(INA226_MASK_EN_REG, value);
+    
 }
 
 void INA226_WE::readAndClearFlags(){
-	uint16_t value = readRegister(INA226_MASK_EN_REG);
-	overflow = (value>>2) & 0x0001;
-	convAlert = (value>>3) & 0x0001;
-	limitAlert = (value>>4) & 0x0001;
+    uint16_t value = readRegister(INA226_MASK_EN_REG);
+    overflow = (value>>2) & 0x0001;
+    convAlert = (value>>3) & 0x0001;
+    limitAlert = (value>>4) & 0x0001;
 }
-	
+    
 
 /************************************************ 
-	private functions
+    private functions
 *************************************************/
 
 void INA226_WE::writeRegister(uint8_t reg, uint16_t val){
-  Wire.beginTransmission(i2cAddress);
+  _wire->beginTransmission(i2cAddress);
   uint8_t lVal = val & 255;
   uint8_t hVal = val >> 8;
-  Wire.write(reg);
-  Wire.write(hVal);
-  Wire.write(lVal);
-  Wire.endTransmission();
+  _wire->write(reg);
+  _wire->write(hVal);
+  _wire->write(lVal);
+  _wire->endTransmission();
 }
   
 uint16_t INA226_WE::readRegister(uint8_t reg){
   uint8_t MSByte = 0, LSByte = 0;
   uint16_t regValue = 0;
-  Wire.beginTransmission(i2cAddress);
-  Wire.write(reg);
-  Wire.endTransmission();
-  Wire.requestFrom(i2cAddress,2);
-  if(Wire.available()){
-    MSByte = Wire.read();
-    LSByte = Wire.read();
+  _wire->beginTransmission(i2cAddress);
+  _wire->write(reg);
+  _wire->endTransmission();
+  _wire->requestFrom(i2cAddress,2);
+  if(_wire->available()){
+    MSByte = _wire->read();
+    LSByte = _wire->read();
   }
   regValue = (MSByte<<8) + LSByte;
   return regValue;
 }
-	
+    
 
 
--- src/INA226_WE.h
+++ src/INA226_WE.h
@@ -28,122 +28,127 @@
 #include <Wire.h>
 
 /* registers */
-#define INA226_ADDRESS  		0x40
-#define INA226_CONF_REG 		0x00 //Configuration Register
-#define INA226_SHUNT_REG    	0x01 //Shunt Voltage Register
-#define INA226_BUS_REG    		0x02 //Bus Voltage Register
-#define INA226_PWR_REG     		0x03 //Power Register 
-#define INA226_CURRENT_REG 		0x04 //Current flowing through Shunt
-#define INA226_CAL_REG     		0x05 //Calibration Register 
-#define INA226_MASK_EN_REG		0x06 //Mask/Enable Register 
-#define INA226_ALERT_LIMIT_REG	0x07 //Alert Limit Register
-#define INA226_MAN_ID_REG		0xFE //Contains Unique Manbufacturer Identification Number
-#define INA226_ID_REG			0xFF //Contains unique ID
+#define INA226_ADDRESS          0x40
+#define INA226_CONF_REG         0x00 //Configuration Register
+#define INA226_SHUNT_REG        0x01 //Shunt Voltage Register
+#define INA226_BUS_REG          0x02 //Bus Voltage Register
+#define INA226_PWR_REG          0x03 //Power Register 
+#define INA226_CURRENT_REG      0x04 //Current flowing through Shunt
+#define INA226_CAL_REG          0x05 //Calibration Register 
+#define INA226_MASK_EN_REG      0x06 //Mask/Enable Register 
+#define INA226_ALERT_LIMIT_REG  0x07 //Alert Limit Register
+#define INA226_MAN_ID_REG       0xFE //Contains Unique Manbufacturer Identification Number
+#define INA226_ID_REG           0xFF //Contains unique ID
 
 /* parameters, flag bits */
-#define INA226_RST            	0x8000 //Reset 
-#define INA226_AFF			   	0x0010 //Alert function flag
-#define INA226_CVRF				0x0008 //Conversion ready flag
-#define INA226_OVF				0x0004 //Overflow flags
-#define INA226_ALERT_POL		0x0002 //Alert pin polarity - if set then active-high
+#define INA226_RST              0x8000 //Reset 
+#define INA226_AFF              0x0010 //Alert function flag
+#define INA226_CVRF             0x0008 //Conversion ready flag
+#define INA226_OVF              0x0004 //Overflow flags
+#define INA226_ALERT_POL        0x0002 //Alert pin polarity - if set then active-high
 //Latch enable - if set then alert flag remains until mask/enable register is read
 //if not set then flag is cleared after next conversion within limits
-#define INA226_LATCH_EN			0x0001 
+#define INA226_LATCH_EN         0x0001 
 
 typedef enum INA226_AVERAGES{
-	AVERAGE_1 		= 0x0000, 
-	AVERAGE_4 		= 0x0200,
-	AVERAGE_16 		= 0x0400,
-	AVERAGE_64		= 0x0600,
-	AVERAGE_128		= 0x0800,
-	AVERAGE_256		= 0x0A00,
-	AVERAGE_512		= 0x0C00,
-	AVERAGE_1024	= 0x0E00
+    AVERAGE_1       = 0x0000, 
+    AVERAGE_4       = 0x0200,
+    AVERAGE_16      = 0x0400,
+    AVERAGE_64      = 0x0600,
+    AVERAGE_128     = 0x0800,
+    AVERAGE_256     = 0x0A00,
+    AVERAGE_512     = 0x0C00,
+    AVERAGE_1024    = 0x0E00
 } averageMode;
 
 typedef enum INA226_CONV_TIME{ // Conversion time in microseconds
-	CONV_TIME_140 	= 0b00000000,
-	CONV_TIME_204 	= 0b00000001,
-	CONV_TIME_332 	= 0b00000010,
-	CONV_TIME_588 	= 0b00000011,
-	CONV_TIME_1100 	= 0b00000100,
-	CONV_TIME_2116 	= 0b00000101,
-	CONV_TIME_4156	= 0b00000110,
-	CONV_TIME_8244	= 0b00000111
+    CONV_TIME_140   = 0b00000000,
+    CONV_TIME_204   = 0b00000001,
+    CONV_TIME_332   = 0b00000010,
+    CONV_TIME_588   = 0b00000011,
+    CONV_TIME_1100  = 0b00000100,
+    CONV_TIME_2116  = 0b00000101,
+    CONV_TIME_4156  = 0b00000110,
+    CONV_TIME_8244  = 0b00000111
 } convTime;
 
 typedef enum INA226_MEASURE_MODE{
-	POWER_DOWN 		= 0b00000000,
-	TRIGGERED		= 0b00000011,
-	CONTINUOUS		= 0b00000111
+    POWER_DOWN      = 0b00000000,
+    TRIGGERED       = 0b00000011,
+    CONTINUOUS      = 0b00000111
 } INA226_measureMode;
 
 typedef enum INA226_ALERT_TYPE{
-	SHUNT_OVER 	  = 0x8000,
-	SHUNT_UNDER	  = 0x4000,
-	BUS_OVER	  = 0x2000,
-	BUS_UNDER	  = 0x1000,
-	POWER_OVER	  = 0x0800,
-	CURRENT_OVER  = 0xFFFE,
-	CURRENT_UNDER = 0xFFFF,
-	//CONV_READY	  = 0x0400   not implemented! Use enableConvReadyAlert() 
+    SHUNT_OVER    = 0x8000,
+    SHUNT_UNDER   = 0x4000,
+    BUS_OVER      = 0x2000,
+    BUS_UNDER     = 0x1000,
+    POWER_OVER    = 0x0800,
+    CURRENT_OVER  = 0xFFFE,
+    CURRENT_UNDER = 0xFFFF,
+    //CONV_READY      = 0x0400   not implemented! Use enableConvReadyAlert() 
 } alertType;
 
 typedef enum INA226_CURRENT_RANGE{
-	MA_400,
-	MA_800
+    MA_400,
+    MA_800
 } currentRange;
 
 class INA226_WE
 {
-public:	
-	INA226_WE(int addr);
-	INA226_WE();			//sets default I2C Address 0x40
+public: 
+// Constructors: if not passed, 0x40 / Wire will be set as address / wire object
+    INA226_WE(int addr);
+    INA226_WE();
+    INA226_WE(TwoWire *w, int addr);
+    INA226_WE(TwoWire *w);
   
-	bool init();
-	void reset_INA226();
-	void setCorrectionFactor(float corr);
-	void setAverage(INA226_AVERAGES averages);
-	void setConversionTime(INA226_CONV_TIME convTime);
-	void setConversionTime(INA226_CONV_TIME shuntConvTime, INA226_CONV_TIME busConvTime);
-	void setMeasureMode(INA226_MEASURE_MODE mode);
-	void setCurrentRange(INA226_CURRENT_RANGE range);
-	void setResistorRange(float resistor, float range);
-	float getShuntVoltage_mV();
-	float getShuntVoltage_V();
-	float getBusVoltage_V();
-	float getCurrent_mA();
-	float getCurrent_A();
-	float getBusPower();
-	void startSingleMeasurement();
-	void startSingleMeasurementNoWait();
-	bool isBusy();
-	void powerDown();
-	void powerUp(); 
-	void waitUntilConversionCompleted();
-	void setAlertPinActiveHigh();
-	void enableAlertLatch();
-	void enableConvReadyAlert();
-	void setAlertType(INA226_ALERT_TYPE type, float limit);
-	void readAndClearFlags();
-	bool overflow;
-	bool convAlert;
-	bool limitAlert;
-	
-	
+    bool init();
+    void reset_INA226();
+    void setCorrectionFactor(float corr);
+    void setAverage(INA226_AVERAGES averages);
+    void setConversionTime(INA226_CONV_TIME convTime);
+    void setConversionTime(INA226_CONV_TIME shuntConvTime, INA226_CONV_TIME busConvTime);
+    void setMeasureMode(INA226_MEASURE_MODE mode);
+    void setCurrentRange(INA226_CURRENT_RANGE range);
+    void setResistorRange(float resistor, float range);
+    float getShuntVoltage_mV();
+    float getShuntVoltage_V();
+    float getBusVoltage_V();
+    float getCurrent_mA();
+    float getCurrent_A();
+    float getBusPower();
+    void startSingleMeasurement();
+    void startSingleMeasurementNoWait();
+    bool isBusy();
+    void powerDown();
+    void powerUp(); 
+    void waitUntilConversionCompleted();
+    void setAlertPinActiveHigh();
+    void enableAlertLatch();
+    void enableConvReadyAlert();
+    void setAlertType(INA226_ALERT_TYPE type, float limit);
+    void readAndClearFlags();
+    bool overflow;
+    bool convAlert;
+    bool limitAlert;
+    
+    
 private:
-	INA226_AVERAGES deviceAverages;
-	INA226_CONV_TIME deviceConvTime;
-	INA226_MEASURE_MODE deviceMeasureMode;
-	INA226_CURRENT_RANGE deviceCurrentRange;
-	INA226_ALERT_TYPE deviceAlertType; 
-	int i2cAddress;
-	uint16_t calVal;
-	uint16_t confRegCopy;
-	float currentDivider_mA;
-	float pwrMultiplier_mW;
-	void writeRegister(uint8_t reg, uint16_t val);
-	uint16_t readRegister(uint8_t reg);
+    INA226_AVERAGES deviceAverages;
+    INA226_CONV_TIME deviceConvTime;
+    INA226_MEASURE_MODE deviceMeasureMode;
+    INA226_CURRENT_RANGE deviceCurrentRange;
+    INA226_ALERT_TYPE deviceAlertType; 
+    TwoWire *_wire;
+    int i2cAddress;
+    uint16_t calVal;
+    float corrFactor;
+    uint16_t confRegCopy;
+    float currentDivider_mA;
+    float pwrMultiplier_mW;
+    void writeRegister(uint8_t reg, uint16_t val);
+    uint16_t readRegister(uint8_t reg);
 };
 
 #endif
