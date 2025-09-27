5eef9bf73a02dcd9a5789fe604b40fa143fe70f8
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2025-09-27 12:32:38 +0200

    Add files via upload

--- examples/PowerDown/PowerDown.ino
+++ examples/PowerDown/PowerDown.ino
@@ -21,7 +21,7 @@
 INA226_WE ina226 = INA226_WE(I2C_ADDRESS);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Wire.begin();
   // default parameters are set - for change check the other examples
   ina226.init();
